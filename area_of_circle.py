324e80f4af19e9768dfbef7abdea896fa461b3fb
Bhupendrasinghrathore01 <84668743+Bhupendrasinghrathore01@users.noreply.github.com>
2022-03-15 00:48:41 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,16 @@
+# This atatement takes the input from user for the radius.
+
+a = input("Enter the radius of the circle here : ")
+a = float(a)
+
+# This is the value of pie.
+pi = 3.14
+pi = float(pi)
+
+# This is the solution for the area calculation of the circle which is pie(radius)^2.
+area = pi*a*a
+area = float(area)
+
+# This statement prints the final result.
+print("The area of the circle with given radius is : ", area)
+
